004469d1bf25c5c61c943263e159f40c8185e194
Mohamad Abyannaufal <abyannaufal027@gmail.com>
2020-06-16 12:47:02 +0700

    UI mhs afterlogin

---
+++ mhs.php
@@ -0,0 +1,117 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title> Mahasiswa </title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" href="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <!--Navbar-->
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+        <div class="container">
+          <a class="navbar-brand" href="#">ABSENKU</a>
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#option"><img src="setting.png"/><span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#logout"><img src="logout.png"/><span class="sr-only">(current)</span></a>
+              </li>
+              <!--<li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#Jadwal" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Jadwal
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Senin</a>
+                  <a class="dropdown-item" href="#">Selasa</a>
+                  <a class="dropdown-item" href="#">Rabu</a>
+                  <a class="dropdown-item" href="#">Kamis</a>
+                  <a class="dropdown-item" href="#">Jumat</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Opsional</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#">Tentang</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>-->
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <!--Table-->
+      <br><br><br>
+      <h1> NAMA MAHASISWA </h1>
+      <h2> NIM </h2>
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">No.</th>
+            <th scope="col">SKS</th>
+            <th scope="col">Mata Kuliah</th>
+            <th scope="col">Kode Kelas</th>
+            <th scope="col">Ruangan</th>
+            <th scope="col">Jadwal</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          <!--Nanti di php, ini di loop buat data output-->
+          <tr>
+            <th scope="row">1</th>
+            <td>$SKS</td>
+            <td>$Matkul</td>
+            <td>$KodeKelas</td>
+            <td>$Ruangan</td>
+            <td>$Jadwal</td>
+          </tr>
+          <!--endloop-->
+
+        </tbody>
+      </table>
+
+      <!--Abaikan ini-->
+      <table class="table">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
+</html>
