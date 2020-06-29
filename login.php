a52487c2d4b40e52a31494a680e37d0ceaa72d7e
Mohamad Abyannaufal <abyannaufal027@gmail.com>
2020-06-29 13:50:47 +0700

    CSS dipisah

--- login.php
+++ login.php
@@ -11,72 +11,7 @@
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" href="bootstrap-css.css">
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-  <style type="text/css">
-    .login-container{
-      margin-top: 5%;
-      margin-bottom: 5%;
-    }
-    .login-logo{
-      position: relative;
-      margin-left: -41.5%;
-    }
-    .login-logo img{
-      position: absolute;
-      width: 20%;
-      margin-top: 19%;
-      background: #0079C6;
-      border-radius: 4.5rem;
-      padding: 5%;
-    }
-    .login-form-1{
-      padding: 9%;
-      background:#DDDDDD;
-      box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 9px 26px 0 rgba(0, 0, 0, 0.19);
-    }
-    .login-form-1 h3{
-      text-align: center;
-      margin-bottom:12%;
-      color:#000000;
-    }
-    .login-form-2{
-      padding: 9%;
-      background: #0079C6;
-      box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 9px 26px 0 rgba(0, 0, 0, 0.19);
-    }
-    .login-form-2 h3{
-      text-align: center;
-      margin-bottom:12%;
-      color: #fff;
-    }
-    .btnSubmit{
-      font-weight: 600;
-      width: 50%;
-      color: #282726;
-      background-color: #fff;
-      border: none;
-      border-radius: 1.5rem;
-      padding:2%;
-    }
-    .btnForgetPwd1{
-      color: #000000;
-      font-weight: 600;
-      text-decoration: none;
-    }
-    .btnForgetPwd2{
-      color: #fff;
-      font-weight: 600;
-      text-decoration: none;
-    }
-    .btnForgetPwd:hover{
-      text-decoration:none;
-      color:#fff;
-    }
-    .carousel-inner{
-      width:100%;
-      max-height: 500px !important;
-    }
-  </style>
+  <link rel="stylesheet" type="text/css" href="style.css">
 
   <title>Absenku LOGIN</title>
 </head>
