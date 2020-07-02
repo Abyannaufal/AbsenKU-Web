3a60b2183b442ca60164f6b6d55aa621865fa413
benedictta <benedictta.dinda@gmail.com>
2020-07-02 13:11:00 +0700

    v 0.3d Alternate2

--- View/view_mhs_absensi.php
+++ View/view_mhs_absensi.php
@@ -16,7 +16,7 @@ $krs = "krs_".strtolower(str_replace(".", "", $nim));
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="view/styles.css">
   </head>
   <body>
       <!--Navbar-->
